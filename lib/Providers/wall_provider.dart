e64cf28b7cc2ee26e1231508c86d4353ec3fac8f
usmansiddiqui12321 <usmansiddiqui12321@gmail.com>
2023-06-20 07:48:26 +0500

    COMMIT

--- lib/Providers/wall_provider.dart
+++ lib/Providers/wall_provider.dart
@@ -1,42 +1,4 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
 
-// import '../Pages/Home/post_components.dart';
-
-// class WallProvider extends ChangeNotifier {
-//   PostComponents postcomp = PostComponents();
-//   bool _isliked = false;
-//   bool get isliked => _isliked;
-//   setisLike() {
-//     _isliked = !_isliked;
-//     notifyListeners();
-//   }
-
-//   islikedInitvalue({required value}) {
-//     _isliked = value;
-//     notifyListeners();
-//   }
-
-//   final currentUser = FirebaseAuth.instance.currentUser;
-
-//   void toggleLike({required postID}) {
-//     setisLike();
-//     DocumentReference postRef =
-//         FirebaseFirestore.instance.collection('User Post').doc(postID);
-//     if (isliked) {
-//       // if post is like add users email to likes array field
-//       postRef.update({
-//         'likes': FieldValue.arrayUnion([currentUser!.email]),
-//       });
-//     } else {
-//       // if it is unliked then remove users email from likes array field
-//       postRef.update({
-//         'likes': FieldValue.arrayRemove([currentUser!.email]),
-//       });
-//     }
-//   }
-// }
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
